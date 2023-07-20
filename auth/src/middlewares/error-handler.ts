e088b69c386727b7cbe243d128549362a308b347
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-07-20 08:40:22 +0100

    convert errors to responses

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,9 +1,24 @@
+// custom error handling middleware
 import { Request, Response, NextFunction } from 'express';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.log('Something went wrong', err);
+    // defining a common error response structure
+    if (err instanceof RequestValidationError) {
+        const formattedErrors = err.errors.map((error) => {
+          if (error.type === 'field') {
+            return { message: error.msg, field: error.path };
+          }
+        });
+        return res.status(400).send({ errors: formattedErrors });
+      }
 
+    if (err instanceof DatabaseConnectionError) {
+        return res.status(500).send({ errors: [{ message: err.reason }]});
+    }
+    // response for generic/unplanned errors
     res.status(400).send({
-        message: err.message
+        errors: [{ message: 'Something went wrong'}]
     });
 };
