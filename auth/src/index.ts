5558f169f96cb011bdc76c72055e1d3cc127acf5
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-07-24 10:08:17 +0100

    new custom error

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,8 +5,8 @@ import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
-
 import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
@@ -16,6 +16,11 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+// handling routes that dont exist
+app.all('*', () => {
+    throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
