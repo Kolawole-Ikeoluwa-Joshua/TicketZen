9899bac4e139dcb6a2c46073f698e4036c2da129
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-07-27 07:56:08 +0100

    password hashing middleware

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { Password } from '../../services/password';
 
 // An interface that describes the properties
 // that are required to create a new User
@@ -31,7 +32,18 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 });
-// custom function built into the model
+
+//password hashing - middleware
+// using mongoose pre-save hook
+userSchema.pre('save', async function(done) {
+    if (this.isModified('password')) {
+        const hashed = await Password.toHash(this.get('password'));
+        this.set('password', hashed);
+    }
+    done();
+});
+
+// statics - custom function built into the model
 userSchema.statics.build = (attrs: UserAttrs) => {
     return new User(attrs);
 };
