f4d4d3812e4db52d1ff31c3d2ccf515ec09bc31a
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-08-01 06:14:27 +0100

    returning current user

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,23 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-    res.send('Hi there!');
+    // check if currentuser has a session
+    if (!req.session?.jwt) {
+        // return early
+        return res.send({ currentUser: null });
+    }
+
+    try {
+        // valid JWT
+        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+        res.send ({ currentUser: payload })
+    } catch (err) {
+        // invalid JWT
+        res.send({ currentUser: null });
+    }
 });
 
 export { router as currentUserRouter };
