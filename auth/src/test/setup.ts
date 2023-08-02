e4964c4b1ec4a14d909e6df6b9591ea95cb4aab8
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-08-02 07:59:03 +0100

    test environment setup

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,27 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import { app } from '../app';
+
+let mongo: any;
+// hook runs before all tests
+beforeAll(async () => {
+    mongo = await MongoMemoryServer.create();
+    const mongoUri = mongo.getUri();
+
+    await mongoose.connect(mongoUri, {});
+});
+// hook runs before each tests
+beforeEach(async () => {
+    const collections = await mongoose.connection.db.collections();
+
+    for (let collection of collections) {
+        await collection.deleteMany({});
+    }
+});
+// hook runs after all tests
+afterAll(async () => {
+    if (mongo) {
+        await mongo.stop();
+    }
+    await mongoose.connection.close();
+});
