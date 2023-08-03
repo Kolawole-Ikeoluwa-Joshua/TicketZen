a356b30d85829dd82e1c6f0392effbdd894ff23e
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-08-03 09:11:45 +0100

    auth cookie helper function

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -2,14 +2,8 @@ import request from 'supertest';
 import { app } from '../../app';
 
 it('respond with details about the current user', async () => {
-    const signupResponse = await request(app)
-        .post('/api/users/signup')
-        .send({
-            email: 'test@test.com',
-            password: 'password'
-        })
-        .expect(201);
-    const cookie = signupResponse.get('Set-Cookie');
+
+    const cookie = await global.signin();
 
     const response = await request(app)
         .get('/api/users/currentuser')
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,12 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import { app } from '../app';
+import request from 'supertest';
+
+// let typescript know there is a global interface with signin property
+declare global {
+    var signin: () => Promise<string[]>;
+}
 
 let mongo: any;
 // hook runs before all tests
@@ -28,3 +34,21 @@ afterAll(async () => {
     }
     await mongoose.connection.close();
 });
+
+// get auth cookie helper function
+global.signin = async () => {
+    const email = 'test@test.com';
+    const password = 'password';
+
+    const response = await request(app)
+        .post('/api/users/signup')
+        .send({
+            email,
+            password
+        })
+        .expect(201);
+
+    const cookie = response.get('Set-Cookie');
+
+    return cookie;
+};
