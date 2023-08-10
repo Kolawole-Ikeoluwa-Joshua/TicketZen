088117adde88b957653334ee0620ec9fd8f5e17d
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-08-10 08:35:29 +0100

    implement SSR

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 const Landing = ({ currentUser }) => {
-    // console.log(currentUser);
+    
     // axios.get('/api/users/currentuser').catch((err) => {
     //     console.log(err.message);
     // });
+    console.log(currentUser);
 
     return <h1>Landing Page</h1>;
 };
@@ -14,9 +15,22 @@ Landing.getInitialProps = async () => {
    if (typeof window === 'undefined') {
         // we are on the server!
         // requests should be made to cross namespace ingress-nginx-ctrlr domain
+        // http://SERVICENAME.NAMESPACE.svc.cluster.local
+        const { data } = await axios.get(
+            'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
+                headers: {
+                    Host: 'ticketzen.dev'
+                }
+            }
+        );
+
+        return data;
    } else {
         // we are on the browser!
         // requests can be made with a base url of ''
+        const { data } = await axios.get('/api/users/currentuser');
+
+        return data;
    }
 
    return {};
