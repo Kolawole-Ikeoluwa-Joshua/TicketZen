bbd5523b1a657e35e2ceb76bbfa294fe8405526f
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-08-10 08:16:12 +0100

    determine if on browser or server

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,19 +1,25 @@
 import axios from 'axios';
 
 const Landing = ({ currentUser }) => {
-    console.log(currentUser);
-    axios.get('/api/users/currentuser').catch((err) => {
-        console.log(err.message);
-    });
+    // console.log(currentUser);
+    // axios.get('/api/users/currentuser').catch((err) => {
+    //     console.log(err.message);
+    // });
 
     return <h1>Landing Page</h1>;
 };
 
 // get extra data before rendering up webpage - used when SSR
-// Landing.getInitialProps = async () => {
-//    const response = await axios.get('/api/users/currentuser');
+Landing.getInitialProps = async () => {
+   if (typeof window === 'undefined') {
+        // we are on the server!
+        // requests should be made to cross namespace ingress-nginx-ctrlr domain
+   } else {
+        // we are on the browser!
+        // requests can be made with a base url of ''
+   }
 
-//    return response.data;
-// };
+   return {};
+};
 
 export default Landing;
