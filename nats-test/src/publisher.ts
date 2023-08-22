2c67003e9882ae65aed00af28823315898f0efb8
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-08-22 08:57:47 +0100

    publishing message or event

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -6,4 +6,15 @@ const stan = nats.connect('ticketzen', 'abc', {
 
 stan.on('connect', () => {
     console.log('Publisher connected to NATS');
+
+    const data = JSON.stringify({
+        id: '123',
+        title: 'concert',
+        price: 20
+    });
+
+    stan. publish('ticket:created', data, () => {
+        console.log('Event published');
+    });
+
 });
