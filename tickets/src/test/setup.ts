0d6395672cae05a8b6ecf229cabbfe18da53dc2f
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-08-24 08:46:20 +0100

    fake authentication for tests

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -2,10 +2,11 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import { app } from '../app';
 import request from 'supertest';
+import jwt from 'jsonwebtoken';
 
 // let typescript know there is a global interface with signin property
 declare global {
-    var signin: () => Promise<string[]>;
+    var signin: () => string[];
 }
 
 let mongo: any;
@@ -36,19 +37,24 @@ afterAll(async () => {
 });
 
 // get auth cookie helper function
-global.signin = async () => {
-    const email = 'test@test.com';
-    const password = 'password';
+global.signin = () => {
+    // Build a JWT payload, { id, email }
+    const payload = {
+        id: 'jfjfifsi',
+        email: 'test@test.com'
+    };
+    // Create the JWT!
+    const token = jwt.sign(payload, process.env.JWT_KEY!);  
 
-    const response = await request(app)
-        .post('/api/users/signup')
-        .send({
-            email,
-            password
-        })
-        .expect(201);
+    // Build session Object. { jwt: MY_JWT }
+    const session = { jwt: token };
 
-    const cookie = response.get('Set-Cookie');
+    // Turn that session into JSON
+    const sessionJSON = JSON.stringify(session);
 
-    return cookie;
+    // Take JSON and encode it as base64
+    const base64 = Buffer.from(sessionJSON).toString('base64');
+
+    // return a string thats the cookie with the encoded data
+    return [`session=${base64}`];
 };
