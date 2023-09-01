0fd866dd3ee675194e61146749828fbcf63bb8ad
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-09-01 09:00:28 +0100

    shows route

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,10 +1,11 @@
 import request from 'supertest';
 import { app } from '../../app';
-
+import mongoose from 'mongoose';
 
 it('returns a 404 if the ticket is not found', async () => {
+    const id = new mongoose.Types.ObjectId().toHexString();
     await request(app)
-        .get('/api/tickets/hhdsdhjjsdjsiugiug')
+        .get(`/api/tickets/${id}`)
         .send()
         .expect(404);
 
