67169540291bff1a5eb9e6717bba5837c8c42cd7
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-09-26 08:17:49 +0100

    update route

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -7,6 +7,7 @@ import { errorHandler, NotFoundError, currentUser } from '@scar-tickets/common';
 import { createTicketRouter } from './routes/new';
 import { showTicketRouter } from './routes/show';
 import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
 
 const app = express();
 
@@ -24,6 +25,7 @@ app.use(currentUser);
 app.use(createTicketRouter);
 app.use(showTicketRouter);
 app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 
 // handling routes that dont exist
