dd62d73ac49e0164450bb01495cc7782d2a22f30
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-09-29 16:31:01 +0100

    tickets route to ingress nginx

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -12,7 +12,7 @@ const start = async () => {
     }
 
     try{
-        await mongoose.connect('process.env.MONGO_URI');
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('Connnected to MongoDB');
     } catch (err) {
         console.error(err);
