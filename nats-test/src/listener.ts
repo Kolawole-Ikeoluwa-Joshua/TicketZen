5d7e81801f2d120b9a5a51cda6a5547a767a3aa7
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-10-03 06:33:16 +0100

    accessing event data

---
+++ nats-test/src/listener.ts
@@ -0,0 +1,25 @@
+import nats, { Message } from 'node-nats-streaming';
+
+console.clear()
+
+const stan = nats.connect('ticketzen', '123', {
+    url: 'http://localhost:4222',
+});
+
+stan.on('connect', () => {
+    console.log('Listener connected to NATS');
+
+    // subscription listens to channel
+    const subscription = stan.subscribe('ticket:created');
+
+    subscription.on('message', (msg: Message) => {
+       const data = msg.getData();
+
+       if (typeof data === 'string') {
+            console.log(
+                `Received event #${msg.getSequence()}, with data: ${data}`
+            );
+       }
+    });
+});
+
--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,5 +1,7 @@
 import nats from 'node-nats-streaming';
 
+console.clear()
+
 const stan = nats.connect('ticketzen', 'abc', {
     url: 'http://localhost:4222',
 });
