fb7d0a1371f81859d42f16ebaa7b1b7d315293d8
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-10-03 05:51:40 +0100

    enable restart on ts-node-dev

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -7,12 +7,14 @@ const stan = nats.connect('ticketzen', 'abc', {
 stan.on('connect', () => {
     console.log('Publisher connected to NATS');
 
+    // nats streaming server only accepts strings as messages
     const data = JSON.stringify({
         id: '123',
         title: 'concert',
         price: 20
     });
 
+    // topic/channel = ticket:created
     stan. publish('ticket:created', data, () => {
         console.log('Event published');
     });
