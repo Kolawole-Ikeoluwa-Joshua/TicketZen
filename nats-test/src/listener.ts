3bebd66e448114d60cac22853532652b5f03ba05
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-10-04 07:58:23 +0100

    extend listener class for tickets events

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,6 @@
-import nats, { Message, Stan } from 'node-nats-streaming';
+import nats from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
+import { TicketCreatedListener } from './events/ticket-created-listener';
 
 console.clear()
 
@@ -16,84 +17,10 @@ stan.on('connect', () => {
         process.exit();
     });
 
-    // customizing subscriptions
-    const options = stan
-        .subscriptionOptions()
-        .setManualAckMode(true)
-        .setDeliverAllAvailable() // event redelivery
-        .setDurableName('example-service'); //durable subscription
+    new TicketCreatedListener(stan).listen();
 
-    // subscription listens to channel
-    // queue group = orders-service-queue-group
-    const subscription = stan.subscribe(
-        'ticket:created', 
-        'orders-service-queue-group',
-        options
-    );
-
-    subscription.on('message', (msg: Message) => {
-       const data = msg.getData();
-
-       if (typeof data === 'string') {
-            console.log(
-                `Received event #${msg.getSequence()}, with data: ${data}`
-            );
-       }
-
-       // manual ack mode - to successfully complete message processing
-       msg.ack();
-
-    });
 });
 
 // listen for interrupt or shutdown signals
 process.on('SIGINT', () => stan.close());
 process.on('SIGTERM', () => stan.close());
-
-
-
-abstract class Listener {
-    abstract subject: string;
-    abstract queueGroupName: string;
-    abstract onMessage(data: any, msg: Message): void;
-    private client: Stan;
-    protected ackWait = 5 * 1000;
-
-    constructor(client: Stan) {
-        this.client = client;
-    }
-
-    subscriptionOptions() {
-        return this.client
-            .subscriptionOptions()
-            .setDeliverAllAvailable()
-            .setManualAckMode(true)
-            .setAckWait(this.ackWait)
-            .setDurableName(this.queueGroupName);
-    }
-
-    listen() {
-        const subscription = this.client.subscribe(
-            this.subject,
-            this.queueGroupName,
-            this.subscriptionOptions()
-        );
-
-        subscription.on('message', (msg: Message) => {
-            console.log(
-                `Message received: ${this.subject} / ${this.queueGroupName}`
-            );
-
-            const parsedData = this.parseMessage(msg);
-            this.onMessage(parsedData, msg);
-
-        });
-    }
-
-    parseMessage(msg: Message) {
-        const data = msg.getData();
-        return typeof data === 'string'
-            ? JSON.parse(data)
-            : JSON.parse(data.toString('utf8'));
-    }
-}
