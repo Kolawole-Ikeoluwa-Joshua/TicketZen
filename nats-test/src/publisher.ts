8111134d64938b24eb9826f759ee0acbcc9237a4
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-10-05 06:32:46 +0100

    await event publication

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,5 +1,6 @@
 import nats from 'node-nats-streaming';
 import { TicketCreatedPublisher } from './events/ticket-created-publisher';
+import { error } from 'console';
 
 console.clear()
 //clientid = abc
@@ -7,15 +8,19 @@ const stan = nats.connect('ticketzen', 'abc', {
     url: 'http://localhost:4222',
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
     console.log('Publisher connected to NATS');
     
     const publisher = new TicketCreatedPublisher(stan);
-    publisher.publish({
-        id: '123',
-        title: 'concert',
-        price: 20
-    });
+    try {
+        await publisher.publish({
+            id: '123',
+            title: 'concert',
+            price: 20
+        });
+    } catch (err) {
+        console.error(err);
+    }
 
     // nats streaming server only accepts strings as messages
     // const data = JSON.stringify({
