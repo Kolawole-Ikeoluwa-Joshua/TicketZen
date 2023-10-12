74955280488caee0a54c22559f8ed1553b0a0ead
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-10-12 05:55:32 +0100

    modify new orders route

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,24 @@
+import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
+import { requireAuth, validateRequest } from '@scar-tickets/common';
+import { body } from 'express-validator'
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-    res.send({});
-});
+router.post(
+    '/api/orders',
+    requireAuth,
+    [
+        body('ticketId')
+            .not()
+            .isEmpty()
+            .custom((input: string) => mongoose.Types.ObjectId.isValid(input)) //checks if the ticketId is a valid MongoDB ObjectId.
+            .withMessage('TicketId must be provided'),
+    ],
+    validateRequest,
+    async (req: Request, res: Response) => {
+        res.send({});
+    }
+);
 
 export { router as newOrderRouter };
