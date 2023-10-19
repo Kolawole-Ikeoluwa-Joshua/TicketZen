ae431ac14f3eece7185e75ab04d2eabbecfaa675
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-10-19 08:58:09 +0100

    Updates

--- common/src/events/subjects.ts
+++ common/src/events/subjects.ts
@@ -2,4 +2,7 @@
 export enum Subjects {
     TicketCreated = 'ticket:created',
     TicketUpdated = 'ticket:updated',
+
+    OrderCreated = 'order:created',
+    OrderCancelled = 'order:cancelled',
 }
