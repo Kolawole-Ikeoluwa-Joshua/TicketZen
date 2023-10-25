187852204a3e571c01735951d568d71534798502
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-10-25 07:57:22 +0100

    mongoose object ID adjustment

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import { Order, OrderStatus  } from "./order";
 
 interface TicketAttrs {
+    id: string;
     title: string;
     price: number;
 }
@@ -36,7 +37,11 @@ const ticketSchema = new mongoose.Schema({
 });
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
-    return new Ticket(attrs);
+    return new Ticket({
+        _id: attrs.id,
+        title: attrs.title,
+        price: attrs.price,
+    });
 };
 // Run query to check all orders. Find an order where the ticket *is* reserved 
 ticketSchema.methods.isReserved = async function() {
