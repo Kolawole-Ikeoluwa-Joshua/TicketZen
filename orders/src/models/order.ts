8e7a77f5d71c64eb9b9ea274103210682111665c
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-11-13 08:00:59 +0100

    mongoose version plugin to orders model

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
 import { OrderStatus } from "@scar-tickets/common";
 import { TicketDoc } from "./ticket";
 
@@ -49,6 +50,8 @@ const orderSchema = new mongoose.Schema({
         }
     }
 });
+orderSchema.set('versionKey', 'version');
+orderSchema.plugin(updateIfCurrentPlugin);
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
     return new Order(attrs);
