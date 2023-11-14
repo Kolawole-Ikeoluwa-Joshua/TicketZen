feaea8a8299ca52eb7f2482b04e69cec20b2fdb5
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-11-14 08:41:19 +0100

    reserving a ticket

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -1,12 +1,28 @@
 import { Listener, OrderCreatedEvent, Subjects } from "@scar-tickets/common";
 import { queueGroupName } from "./queue-group-name";
 import { Message } from "node-nats-streaming";
+import { Ticket } from "../../models/ticket";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     subject: Subjects.OrderCreated = Subjects.OrderCreated;
     queueGroupName = queueGroupName;
 
     async onMessage(data: OrderCreatedEvent['data'], msg: Message){
-        
+        // find ticket that the order is reserving
+        const ticket = await Ticket.findById(data.ticket.id);
+
+        // if no ticket, throw error
+        if (!ticket) {
+            throw new Error('Ticket not found');
+        }
+
+        // mark ticket as reserved by setting orderId property
+        ticket.set({ orderId: data.id });
+
+        // save the ticket
+        await ticket.save();
+
+        // ack the message
+        msg.ack();
     }
 }
