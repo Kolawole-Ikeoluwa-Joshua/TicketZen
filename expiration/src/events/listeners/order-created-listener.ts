bc198bc46e2bfc4e6cdda48bb9b49aa4f56f4378
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-11-22 08:02:47 +0100

    delaying job processing

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -8,10 +8,18 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     queueGroupName = queueGroupName;
 
     async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+        const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+        console.log('Waiting for this milliseconds to process the job:', delay);
+
         // enqueue a job
-        await expirationQueue.add({
-            orderId: data.id
-        });
+        await expirationQueue.add(
+            {
+                orderId: data.id
+            }, 
+            {
+                delay,
+            }
+        );
 
         msg.ack();
     }
