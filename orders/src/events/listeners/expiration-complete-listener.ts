bab9564b6457148663f1cdfb88f7e8359784d2bc
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-11-24 07:20:11 +0100

    emit order cancelled event

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -2,6 +2,7 @@ import { Listener, Subjects, ExpirationCompleteEvent, OrderStatus } from "@scar-
 import { Message } from "node-nats-streaming";
 import { queueGroupName } from "./queue-group-name";
 import { Order } from "../../models/order";
+import { OrderCancelledPublisher } from "../publishers/order-cancelled-publisher";
 
 export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
     queueGroupName = queueGroupName;
@@ -19,5 +20,18 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
         order.set({
             status: OrderStatus.Cancelled,
         })
+        await order.save();
+
+        // emit order cancelled event
+        await new OrderCancelledPublisher(this.client).publish({
+             id: order.id,
+             version: order.version,
+             ticket: {
+                id: order.ticket.id
+            },
+        });
+
+        msg.ack();
+
     }
 }
