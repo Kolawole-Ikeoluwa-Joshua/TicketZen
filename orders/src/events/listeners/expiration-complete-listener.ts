b97811c16865aa49c41492dd52839b03e9685b6e
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-11-27 18:36:57 +0100

    listening for expiration

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -11,7 +11,7 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
     async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
         // order should be cancelled logic
 
-        const order = await Order.findById(data.orderId);
+        const order = await Order.findById(data.orderId).populate('ticket');
 
         if (!order) {
             throw new Error('Order not found');
