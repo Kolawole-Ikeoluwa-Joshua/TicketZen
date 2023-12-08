32d8555a920abc8b0d747fd63ac7ba3193222c95
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-12-08 08:53:38 +0100

    validate order before payment

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -4,7 +4,9 @@ import {
     requireAuth,
     validateRequest,
     BadRequestError,
-    NotFoundError
+    NotAuthorizedError,
+    NotFoundError,
+    OrderStatus
 } from '@scar-tickets/common'
 import { Order } from '../models/order';
 
@@ -18,7 +20,22 @@ router.post('/api/payments',
     ],
     validateRequest,
     async (req: Request, res: Response) => {
-    res.send({ success: true });
+        const { token, orderId } = req.body;
+        
+        // validate order
+        const order = await Order.findById(orderId);
+
+        if (!order) {
+            throw new NotFoundError();
+        }
+        if (order.userId !== req.currentUser!.id) {
+            throw new NotAuthorizedError();
+        }
+        if (order.status === OrderStatus.Cancelled) {
+            throw new BadRequestError('Cannot pay for a cancelled order');
+        }
+
+        res.send({ success: true });
 });
 
 export { router as createChargeRouter }
