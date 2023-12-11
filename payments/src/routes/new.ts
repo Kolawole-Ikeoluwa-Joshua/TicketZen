1d0f7eacdeaef76d3f15945bcef7f08e2d4a3ef5
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-12-11 08:30:29 +0100

    create a charge with stripe

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -9,6 +9,7 @@ import {
     OrderStatus
 } from '@scar-tickets/common'
 import { Order } from '../models/order';
+import { stripe } from '../stripe';
 
 const router = express.Router();
 
@@ -34,6 +35,13 @@ router.post('/api/payments',
         if (order.status === OrderStatus.Cancelled) {
             throw new BadRequestError('Cannot pay for a cancelled order');
         }
+
+        await stripe.charges.create({
+            currency: 'usd',
+            // convert amoount into cents: x * 100
+            amount: order.price * 100,
+            source: token,
+        });
 
         res.send({ success: true });
 });
