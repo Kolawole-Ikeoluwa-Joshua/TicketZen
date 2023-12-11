6f6b3ca009b3feac0f8a1b572d39380aee8697d0
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-12-11 07:51:53 +0100

    test order validation before payment

---
+++ payments/src/routes/__test__/new.test.ts
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+import request from 'supertest';
+import { OrderStatus } from '@scar-tickets/common';
+import { app } from '../../app';
+import { Order } from '../../models/order';
+
+it('returns a 404 when purchasing an order that does not exist', async () => {
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', global.signin())
+    .send({
+      token: 'asldkfj',
+      orderId: new mongoose.Types.ObjectId().toHexString(),
+    })
+    .expect(404);
+});
+
+it('returns a 401 when purchasing an order that doesnt belong to the user', async () => {
+  const order = Order.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    userId: new mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    price: 20,
+    status: OrderStatus.Created,
+  });
+  await order.save();
+
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', global.signin())
+    .send({
+      token: 'asldkfj',
+      orderId: order.id,
+    })
+    .expect(401);
+});
