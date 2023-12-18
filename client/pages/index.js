6207699dd00d7d33e60177a875419c1321aa7f47
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-12-18 03:25:16 +0100

    listing all tickets

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,15 +1,37 @@
-const Landing = ({ currentUser }) => {
-    return currentUser ? (
-        <h1>You are signed in</h1>
-    ) : (
-        <h1>You are NOT signed in</h1>
+const Landing = ({ currentUser, tickets }) => {
+    const ticketList = tickets.map(ticket => {
+        return (
+            <tr key={tickets.id}>
+                <td>{ticket.title}</td>
+                <td>{ticket.price}</td>
+            </tr>
+        );
+    });
+
+    return (
+        <div>
+            <h1>Tickets</h1>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {ticketList}
+                </tbody>
+            </table>
+        </div>
     );
 };
 
 // get extra data before rendering up webpage - used when SSR
 // information fetching for individual pages
 Landing.getInitialProps = async (context, client, currentUser) => {
-    return {};
+    const { data } = await client.get('/api/tickets');
+
+    return { tickets: data };
 };
 
 export default Landing;
