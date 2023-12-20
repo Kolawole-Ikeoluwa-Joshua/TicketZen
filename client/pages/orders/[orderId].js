d3ce05aa9e5b9980bd30c3b4cc553bf1151ced33
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-12-20 12:15:09 +0100

    rendering user orders list

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 import useRequest from '../../hooks/use-request';
+import Router from 'next/router';
 
 const OrderShow = ({ order, currentUser }) => {
     const [timeLeft, setTimeLeft] = useState(0);
@@ -10,7 +11,7 @@ const OrderShow = ({ order, currentUser }) => {
         body: {
             orderId: order.id
         },
-        onSuccess: (payment) => console.log(payment)
+        onSuccess: () => Router.push('/orders'),
     });
 
     useEffect(() => {
